cf376f314419a38e72cf968dec9c2a3266a4012e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 17:34:25 +0300

    - Define view 'index'.

--- email_sender_app/views.py
+++ email_sender_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http.response import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+def index(request):
+    html_message = loader.render_to_string(
+        'email_proj/message.html',
+        {
+            'name': 'Habari',
+            'body':  'Umepokea barua pepe hii ya kifahari!',
+        })    
+    send_mail(
+        'Hongera!',
+        'Umebahatika kupokea barua hii.',
+        'mbarikiwa@gmail.com',
+        ['mbarikiwa@gmail.com'],
+        html_message=html_message,
+        fail_silently=False,
+    )
+
+    return HttpResponse("Barua Imetumwa!!")
