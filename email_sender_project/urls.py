9c7c0d125aec081ab605bd1c503d754cf7d9c30a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 20:06:10 +0300

    - Include all 'urls' for app 'email_sender_app'.

--- email_sender_project/urls.py
+++ email_sender_project/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from email_sender_app.apps import EmailSenderAppConfig
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(EmailSenderAppConfig.name+'.urls', namespace=EmailSenderAppConfig.name)),
 ]
