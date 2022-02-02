f212914fa810fbc1483d08deb68146655ada3ce3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 17:09:42 +0300

    - Break up Django settings into multiple settings.

--- email_sender_project/wsgi.py
+++ email_sender_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_sender_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      os.environ.get('DJANGO_SETTINGS_MODULE'))
 
 application = get_wsgi_application()
