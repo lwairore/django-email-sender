47c98c02bbc8761cc88daafcf399fab06db0821b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 20:04:27 +0300

    - Map view 'index' to a 'URLConf'.

--- email_sender_app/urls.py
+++ email_sender_app/urls.py
@@ -1,6 +1,10 @@
+from email_sender_app.views import index
+from django.urls import path
 from email_sender_app.apps import EmailSenderAppConfig
 
 
 app_name = EmailSenderAppConfig.name
 
-urlpatterns = []
+urlpatterns = [
+        path('', index, name='send_email'),
+]
